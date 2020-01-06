9d7e3f7982bf3be85cd7b5608b31e80ee22da064
kimja7045 <kimja7045@naver.com>
2020-01-06 11:38:31 +0900

    Updates

---
+++ precache-manifest.f29a274c4857012bb227555d46141be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1550ddfc474d38b11b861aaab0739e1c",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "4d39ebb3f3a68bf0eba0",
+    "url": "/React-TodoList/static/css/main.95d63748.chunk.css"
+  },
+  {
+    "revision": "0fb0e32da5c26498595d",
+    "url": "/React-TodoList/static/js/2.a6622aa5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-TodoList/static/js/2.a6622aa5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d39ebb3f3a68bf0eba0",
+    "url": "/React-TodoList/static/js/main.523acb68.chunk.js"
+  },
+  {
+    "revision": "9c9ca430a6867d9558ac",
+    "url": "/React-TodoList/static/js/runtime-main.d76b7705.js"
+  }
+]);
